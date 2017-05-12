0eb7c71745b061ba2d8c5554523c9f4c8bbe1079
RiverOliveira <rivelynooliveira@gmail.com>
2017-05-12 18:57:55 -0300

    Configurando o acesso ao banco, e adicionando parametros para o escaneamento de entidades e componentes feito pelo spring

--- BlueBankPag/src/main/java/br/com/avaliacao/bluebank/BlueBankPag/BlueBankPagApplication.java
+++ BlueBankPag/src/main/java/br/com/avaliacao/bluebank/BlueBankPag/BlueBankPagApplication.java
@@ -2,7 +2,15 @@ package br.com.avaliacao.bluebank.BlueBankPag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
+@ComponentScan(basePackages = "br.com.avaliacao.bluebank")
+@EnableJpaRepositories(basePackages = "br.com.avaliacao.bluebank")
+@EntityScan(basePackages = "br.com.avaliacao.bluebank")
 @SpringBootApplication
 public class BlueBankPagApplication {
 
@@ -10,3 +18,10 @@ public class BlueBankPagApplication {
 		SpringApplication.run(BlueBankPagApplication.class, args);
 	}
 }
+
+
+
+
+
+
+
