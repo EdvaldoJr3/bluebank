26d8c6d0f34096fb49bbd29535c96c0d6a68c644
RiverOliveira <rivelynooliveira@gmail.com>
2017-05-12 15:57:06 -0300

    Commit inicial

---
+++ BlueBankPag/src/main/java/br/com/avaliacao/bluebank/BlueBankPag/BlueBankPagApplication.java
@@ -0,0 +1,12 @@
+package br.com.avaliacao.bluebank.BlueBankPag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankPagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBankPagApplication.class, args);
+	}
+}
