2a761c88bfb323b653b6d76506c24f56005b9ce5
RiverOliveira <rivelynooliveira@gmail.com>
2017-05-13 13:33:18 -0300

    Adicionando repositório para modelos restantes

---
+++ BlueBankPag/src/main/java/br/com/avaliacao/bluebank/repository/ViewHistoricoTransacaoRepository.java
@@ -0,0 +1,9 @@
+package br.com.avaliacao.bluebank.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.avaliacao.bluebank.model.ViewHistoricoTransacao;
+
+public interface ViewHistoricoTransacaoRepository extends JpaRepository<ViewHistoricoTransacao, Long> {
+
+}
