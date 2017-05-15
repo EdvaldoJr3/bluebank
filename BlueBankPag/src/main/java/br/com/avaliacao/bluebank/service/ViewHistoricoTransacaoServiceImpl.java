1350405c6db6651b41ed7760c038a09aff4d21f1
RiverOliveira <rivelynooliveira@gmail.com>
2017-05-15 15:19:15 -0300

    Criando service para a view de historico de transacao

---
+++ BlueBankPag/src/main/java/br/com/avaliacao/bluebank/service/ViewHistoricoTransacaoServiceImpl.java
@@ -0,0 +1,25 @@
+package br.com.avaliacao.bluebank.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.avaliacao.bluebank.dao.ViewHistoricoTransacaoDao;
+import br.com.avaliacao.bluebank.model.ContaCorrente;
+import br.com.avaliacao.bluebank.model.ViewHistoricoTransacao;
+
+@Service("viewHistoricoTransacaoService")
+public class ViewHistoricoTransacaoServiceImpl implements ViewHistoricoTransacaoService {
+
+	@Autowired
+	private ViewHistoricoTransacaoDao viewHistoricoTransacaoDao;
+
+	@Override
+	public Collection<ViewHistoricoTransacao> findByConta(ContaCorrente conta) {
+		// TODO Auto-generated method stub
+		return viewHistoricoTransacaoDao.findByConta(conta);
+	}
+
+
+}
